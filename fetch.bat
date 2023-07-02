0c9dde0c0648aa720518c7e66ed40ce34afb595e
Li-yang Chen <liyoungc@pm.me>
2023-07-02 15:41:48 +0000

    fix

--- fetch.bat
+++ fetch.bat
@@ -1,20 +1,23 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Read each line from the CSV file
 for /f "tokens=1,2 delims=," %%a in (podcasts.csv) do (
+    :: Remove spaces from the podcast name
     set "name=%%a"
-    set "url=%%b"
+    set "name=!name: =!"
 
-    rem Trim leading whitespace from the url
-    for /f "tokens=*" %%c in ("!url!") do set "url=%%c"
-
-    rem Create the directory if it doesn't exist and navigate into it
+    :: Create a directory for the podcast if it doesn't exist
     if not exist "!name!" mkdir "!name!"
+    
+    :: Change to the podcast directory
     cd "!name!"
 
-    rem Download the podcast
-    youtube-dl -o "%%(playlist_index)s-%%(title)s.%%(ext)s" --playlist-reverse --extract-audio --audio-format mp3 "!url!"
+    :: Download the podcast using youtube-dl
+    youtube-dl -o "%%(playlist_index)s-%%(title)s.%%(ext)s" --playlist-reverse --extract-audio --audio-format mp3 "%%b"
 
-    rem Navigate back to the parent directory
+    :: Change back to the parent directory
     cd ..
 )
+
+endlocal
