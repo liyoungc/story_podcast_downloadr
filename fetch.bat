5ec1bc1b675cc7ba81d8852a7b61de884cc45f24
Li-yang Chen <liyoungc@pm.me>
2023-07-02 22:34:16 +0000

    fix

--- fetch.bat
+++ fetch.bat
@@ -3,8 +3,11 @@ setlocal enabledelayedexpansion
 
 :: Read each line from the CSV file
 for /f "tokens=1,2 delims=," %%a in (podcasts.csv) do (
+    :: Remove leading and trailing spaces from the podcast name and URL
+    set "name=%%~a"
+    set "url=%%~b"
+    
     :: Remove spaces from the podcast name
-    set "name=%%a"
     set "name=!name: =!"
 
     :: Create a directory for the podcast if it doesn't exist
@@ -14,7 +17,7 @@ for /f "tokens=1,2 delims=," %%a in (podcasts.csv) do (
     cd "!name!"
 
     :: Download the podcast using youtube-dl
-    youtube-dl -o "%%(playlist_index)s-%%(title)s.%%(ext)s" --playlist-reverse --extract-audio --audio-format mp3 "%%b"
+    youtube-dl -o "%%(playlist_index)s-%%(title)s.%%(ext)s" --playlist-reverse --extract-audio --audio-format mp3 "!url!"
 
     :: Change back to the parent directory
     cd ..
